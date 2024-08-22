a99163887e4704d59eb75c0d040f5db7fe71ef23
Jai Kiran Pampana <jaikirandbz@gmail.com>
2024-08-22 10:08:49 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,19 +1,6 @@
-package Jpre;
-
-public class Demo {
-
-	public static void main(String[] args) {
-		String exampleString = "Hello, World!";
-
-		int length = exampleString.length();
-		System.out.println("length of the string: " + length);
-
-		char charAtIndex = exampleString.charAt(7);
-		System.out.println("character at index 7: " + charAtIndex);
-
-		String substring = exampleString.substring(7, 12);
-		System.out.println("Substring from index 7 to 12: " + substring);
-
-	}
-
+import java.util.*;
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hi ");
+    }
 }
